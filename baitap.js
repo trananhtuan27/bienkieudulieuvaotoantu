a078f7bd8b1dbeef782182bd0201e3a924b77e29
trananhtuan27 <trananhtuan2710201@gmail.com>
2019-08-02 15:58:52 +0700

    bai1

---
+++ baitap.js
@@ -0,0 +1,5 @@
+let vatly = parseInt(prompt("nhập điểm vật lý"));
+let hoahoc = parseInt(prompt("nhập điểm môn hóa học"));
+let sinhhoc = parseInt(prompt("nhập điểm sinh hóa học"));
+let tong = vatly + hoahoc +sinhhoc;
+document.write(tong)
